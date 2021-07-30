1f9167c580792df7fa12321b271e8645fffdbe26
cristian mosquera <cmosqbone1@gmail.com>
2021-07-31 00:03:50 +0200

    add template dinamic with javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,44 @@
 import "./scss/styles.scss";
+const container = document.querySelector(".card-wrapper");
+const fragment = document.createDocumentFragment();
+const template = document.querySelector("#template-card").content;
 
-// import "../node_modules/reveal.js/dist/reveal.css";
-// import "../node_modules/reveal.js/dist/theme/black.css";
+function getApi() {
+    const URI = `https://api.themoviedb.org/3/search/movie?api_key=b9ddfc9d41f7d2d3fc81bd6280d7db38&language=en-US&query=dark&include_adult=false`;
+
+    fetch(URI)
+        .then((res) => {
+            if (!res === "ok") {
+                throw new Error(`status : ${status}`);
+            }
+
+            return res.json();
+        })
+        .then((data) => {
+            const { results } = data;
+
+            setHtml(results);
+        })
+        .catch((err) => console.log(err));
+}
+
+getApi();
+
+function setHtml(data) {
+    console.log(data);
+    data.forEach((item) => {
+        const { id, title, vote_average: rating, poster_path: img } = item;
+
+        template.querySelector(
+            ".card__img"
+        ).src = `https://image.tmdb.org/t/p/w500${img}`;
+        template.querySelector(".card__img").alt = title;
+        template.querySelector(".card__title").textContent = title;
+        template.querySelector(".card__rating").textContent = rating;
+        const clone = document.importNode(template, true);
+
+        fragment.appendChild(clone);
+    });
+
+    container.appendChild(fragment);
+}
